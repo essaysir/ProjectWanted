0332b3ace99b12e3db811e2b97eb6faacf9aa259
Sonjusun <sonjs7554@naver.com>
2023-05-26 16:17:03 +0900

    mypage + resume_input 만들고 push

--- src/main/java/com/spring/wanted/ProjectWanted/member/controller/MemberController.java
+++ src/main/java/com/spring/wanted/ProjectWanted/member/controller/MemberController.java
@@ -1,27 +1,15 @@
 package com.spring.wanted.ProjectWanted.member.controller;
 
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import com.spring.wanted.ProjectWanted.member.mapper.InterMemberMapper;
 
 @Controller
 public class MemberController {
 	
+		
+
 	
 	
-	
-	// @PostMapping
 	
 	
 	
