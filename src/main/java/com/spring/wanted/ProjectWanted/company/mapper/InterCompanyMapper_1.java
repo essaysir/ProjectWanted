40c13f9a7d3a00b77d4178677a5519b234d151a2
Sonjusun <sonjs7554@naver.com>
2023-05-26 16:01:13 +0900

    기초 설정 중

---
+++ src/main/java/com/spring/wanted/ProjectWanted/company/mapper/InterCompanyMapper_1.java
@@ -0,0 +1,8 @@
+package com.spring.wanted.ProjectWanted.company.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface InterCompanyMapper_1 {
+
+}
--- src/main/java/com/spring/wanted/ProjectWanted/member/controller/MemberController.java
+++ src/main/java/com/spring/wanted/ProjectWanted/member/controller/MemberController.java
@@ -1,28 +1,32 @@
 package com.spring.wanted.ProjectWanted.member.controller;
 
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.spring.wanted.ProjectWanted.member.mapper.InterMemberMapper;
 
 @Controller
 public class MemberController {
 	
-	@Autowired
-	InterMemberMapper mapper ;
 	
-	@RequestMapping(value = "/home", method = RequestMethod.GET)
-    public String home2(HttpServletRequest request) {
-		
-		// int n = mapper.test_insert();
-		String tagname  = mapper.test_select();
-		request.setAttribute("tagname", tagname);
-		
-        // System.out.println(n);
-		return "home.tiles1";
-    }
+	
+	
+	// @PostMapping
+	
+	
+	
+	
+	
+	
+	
 }
