c02768e382f2adf685b5b383e19b3b4ec6b6083c
yunhwan <yunhwan@localhost>
2023-05-31 23:23:54 +0900

    작성 완료 버튼 클릭시 작동하는 유효성 검사 수정(조건문 변경 및 코드 축약) + 컨트롤러 경로 수정

--- src/main/java/com/spring/wanted/ProjectWanted/member/controller/MemberController2.java
+++ src/main/java/com/spring/wanted/ProjectWanted/member/controller/MemberController2.java
@@ -4,7 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.spring.wanted.ProjectWanted.member.service.InterMemberService2;
@@ -20,7 +20,7 @@ public class MemberController2 {
 		this.service2 = service2;
 	}
 
-	@PostMapping(value = "/myresume")
+	@GetMapping(value = "/myresume")
 	public String resume_input(HttpServletRequest request) {
 
 		
