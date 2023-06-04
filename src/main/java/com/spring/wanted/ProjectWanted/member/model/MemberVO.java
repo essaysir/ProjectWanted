8cfb7240cf94c6a120aa4c48407e629ca7574ab4
yunhwan <yunhwan@localhost>
2023-06-04 19:28:46 +0900

    이력서 작성페이지 앞단 완료(임시저장기능은 다음에 할 예정) + MemberVO 생성, 이외의 필요한 VO생성 예정. 중간 저장용 ommit임.

---
+++ src/main/java/com/spring/wanted/ProjectWanted/member/model/MemberVO.java
@@ -0,0 +1,20 @@
+package com.spring.wanted.ProjectWanted.member.model;
+
+import lombok.Data;
+
+@Data
+public class MemberVO {
+	
+    private String userId;
+    private String pwd;
+    private String name;
+    private String nickname;
+    private String mobile;
+    private String profileImage;
+    private int resumeCount;
+    
+    
+    
+    
+}
+
