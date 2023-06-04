553c38fa4f5eaaf5cde16498e954f4587935bf99
yunhwan <yunhwan@localhost>
2023-06-04 20:38:03 +0900

    MemberVO 추가

---
+++ src/main/java/com/spring/wanted/ProjectWanted/member/model/MemberVO.java
@@ -0,0 +1,15 @@
+package com.spring.wanted.ProjectWanted.member.model;
+
+import lombok.Data;
+
+@Data
+public class MemberVO {
+
+	private String userId;
+    private String pwd;
+    private String name;
+    private String nickname;
+    private String mobile;
+    private String profile_Image;
+    private int resume_Count;
+}
