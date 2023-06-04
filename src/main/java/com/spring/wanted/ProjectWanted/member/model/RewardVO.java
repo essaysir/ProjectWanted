6da83e7069f8e3eec0765c3f869f98c57a2f1458
yunhwan <yunhwan@localhost>
2023-06-04 20:33:41 +0900

    VO 재작성하여 커밋.

---
+++ src/main/java/com/spring/wanted/ProjectWanted/member/model/RewardVO.java
@@ -0,0 +1,13 @@
+package com.spring.wanted.ProjectWanted.member.model;
+
+import lombok.Data;
+
+@Data
+public class RewardVO {
+
+	private int reward_Code;
+    private int fk_Resume_Code;
+    private String reward;
+    private String content;
+    private String reward_Date;
+}
