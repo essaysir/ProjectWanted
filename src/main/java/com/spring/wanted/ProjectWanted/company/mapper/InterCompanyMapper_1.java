9f6c2d0e5ba932016403aa8ca2ef819210ddc752
parkes418 <eunseo04_18@naver.com>
2023-06-04 23:46:48 +0900

    PES_중간commit

--- src/main/java/com/spring/wanted/ProjectWanted/company/mapper/InterCompanyMapper_1.java
+++ src/main/java/com/spring/wanted/ProjectWanted/company/mapper/InterCompanyMapper_1.java
@@ -11,4 +11,14 @@ public interface InterCompanyMapper_1 {
 	// 회사 지원자List 불러오기
 	List<Map<String, String>> getcandidateList(Map<String, String> paraMap);
 
+	// 지원자List totalCount
+	int getTotalCount(Map<String, String> paraMap);
+
+	// 페이징처리  한 지원자List 불러오기
+	List<Map<String, String>> candidateListSearchWithPaging(Map<String, String> paraMap);
+
+	// 검색기능
+	List<String> wordSearchShow(Map<String, String> paraMap);
+	
+
 }
