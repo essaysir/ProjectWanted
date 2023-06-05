6158dae01b22fd66daf1d4e54cbfa95f26efc5b3
yunhwan <yunhwan@localhost>
2023-06-05 19:36:48 +0900

    resume_input 스크립트 완료(임시저장 기능은 미구현) VO 및 컨트롤러 업데이

---
+++ src/main/java/com/spring/wanted/ProjectWanted/member/model/MemberTechVO.java
@@ -0,0 +1,12 @@
+package com.spring.wanted.ProjectWanted.member.model;
+
+import lombok.Data;
+
+@Data
+public class MemberTechVO {
+
+	private int memberTechCode;
+    private int techCode;
+    private String userId;
+    private int resumeCode;
+}
