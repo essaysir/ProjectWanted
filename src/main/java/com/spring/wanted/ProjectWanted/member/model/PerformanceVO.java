39123ba474b16ab148e0ca54867865f4adbf344a
parkes418 <eunseo04_18@naver.com>
2023-06-07 16:52:03 +0900

    PES_PerformanceVO

---
+++ src/main/java/com/spring/wanted/ProjectWanted/member/model/PerformanceVO.java
@@ -0,0 +1,15 @@
+package com.spring.wanted.ProjectWanted.member.model;
+
+import lombok.Data;
+
+@Data
+public class PerformanceVO {
+
+	private int perform_code;
+    private int fk_career_code;
+    private int fk_resume_code;
+    private String perform_content;
+    private String start_date;
+    private String end_date;
+    
+}
