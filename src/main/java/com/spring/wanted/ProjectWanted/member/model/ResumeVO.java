0afde8f3b597dc53cbf32bbeef85078c7e023536
yunhwan <yunhwan@localhost>
2023-06-08 14:51:07 +0900

    vo 추가(주요성과VO )

--- src/main/java/com/spring/wanted/ProjectWanted/member/model/ResumeVO.java
+++ src/main/java/com/spring/wanted/ProjectWanted/member/model/ResumeVO.java
@@ -23,5 +23,6 @@ public class ResumeVO {
     private List<SchoolVO> schoolvo; // School 테이블에 대응하는 VO 리스트
     private List<CareerVO> careervo; // Career 테이블에 대응하는 VO 리스트
     private List<MemberTechVO> member_Techvo; // MemberTech 테이블에 대응하는 VO 리스트
+    private List<PerformanceVO> performancevo; // MemberTech 테이블에 대응하는 VO 리스트
 }
 
