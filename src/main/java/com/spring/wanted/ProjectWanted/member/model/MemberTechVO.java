2e766928f6e25c7f5a8a6a3e492b4b6a9f554dd9
yunhwan <yunhwan@localhost>
2023-06-08 14:37:31 +0900

    MemberTechVO수정

--- src/main/java/com/spring/wanted/ProjectWanted/member/model/MemberTechVO.java
+++ src/main/java/com/spring/wanted/ProjectWanted/member/model/MemberTechVO.java
@@ -5,8 +5,8 @@ import lombok.Data;
 @Data
 public class MemberTechVO {
 
-	private int memberTechCode;
-    private int techCode;
-    private String userId;
-    private int resumeCode;
+	private int member_Tech_Code;
+    private int fk_tech_Code;
+    private String fk_userId;
+    private int resume_Code;
 }
