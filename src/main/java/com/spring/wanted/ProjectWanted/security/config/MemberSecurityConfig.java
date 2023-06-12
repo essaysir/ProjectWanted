2ed352d5c1a0e8f9fdd6c6548c77879126969079
Sonjusun <sonjs7554@naver.com>
2023-06-12 16:28:38 +0900

    메인 페이지 수정

--- src/main/java/com/spring/wanted/ProjectWanted/security/config/MemberSecurityConfig.java
+++ src/main/java/com/spring/wanted/ProjectWanted/security/config/MemberSecurityConfig.java
@@ -16,7 +16,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class MemberSecurityConfig  {
 	
-	 @Bean
+		@Bean
 		public  PasswordEncoder passwordEncoder() {
 			return PasswordEncoderFactories.createDelegatingPasswordEncoder();
 		}
